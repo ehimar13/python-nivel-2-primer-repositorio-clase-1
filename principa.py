8ebbc7070aa8b13045c9ebcc72575dab411490f2
Ehimar Vargas <ehimar13@gmail.com>
2025-12-08 18:15:42 -0500

    Update principa.py

--- principa.py
+++ principa.py
@@ -1,1 +1,14 @@
 
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("tu palabra no esta en el diccionario")
+    # ¿Qué hacer si no se encuentra la palabra?
