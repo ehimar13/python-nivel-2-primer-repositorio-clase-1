bf2c9bd2349ccc12668e8d7c9e45da7ca6061a49
Ehimar Vargas <ehimar13@gmail.com>
2025-12-29 18:14:37 -0500

    Remove password generation and add assistance comments
    Removed password generation code and added user assistance comments.

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,20 +1,7 @@
-###
-import random
-caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+#asistencia
 
-# Crea una variable y pide al usuario que introduzca la longitud de la contraseña
-length = int(input("que longitud quieres que tenga la contranseña? "))
-
-# Crea una variable en la que el programa almacenará la contraseña generada
-clave = ""
-
-# Utiliza un bucle y la librería random para seleccionar un carácter 
-# aleatorio de la variable character y añadirlo a la variable con 
-# la contraseña generada
-
-
-for i in range(length):
-    simbolo = random.choice(caracteres)
-    clave += simbolo
-
-print("la contrasena generada es", clave)
+#eliud
+#pedro
+#isaias
+#mathias A
+#mathias S
